5a73cabaeed95d8a7166613b7808c88df2913bf5
Sarah Walz <spwalz@stu.naperville203.org>
2018-12-06 12:22:09 -0600

    Stuck in shell

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -3,7 +3,11 @@
 #Ch.1 "Hello World"
 
 from flask import Flask
+from config import Config
+
 app= Flask(__name__)
+app.config.from_object(Config)
+
 from app import routes #routes is a module, written next
 #import at bottom, don't want secular independcies
 
