f3a011365bcc9449f1a5aaccd9892945ffe35f6f
Sarah Walz <spwalz@stu.naperville203.org>
2018-12-10 12:20:41 -0600

    Got further, it won't compile- but I just want to learn at this point

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app import app
 #first app- is an packat, second app- is the varible defiined other packag
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -22,3 +23,7 @@ def index():
         
     return  render_template('index.html', title= 'Home',  user=user, posts=posts)
 
+@app.routes('/login')
+def login():
+    form= LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
