015f00785d0aeded7aa672aa5a0ba1bbeb136fd8
Sarah Walz <spwalz@stu.naperville203.org>
2018-12-11 12:05:48 -0600

    Finished all of Chapter 3

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
 #first app- is an packat, second app- is the varible defiined other packag
 from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -23,7 +24,11 @@ def index():
         
     return  render_template('index.html', title= 'Home',  user=user, posts=posts)
 
-@app.routes('/login')
+@app.routes('/login', methods=['GET', 'POST'])
 def login():
     form= LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+              form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
